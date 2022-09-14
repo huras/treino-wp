7b23531a924ed6e8bada66d5d025366a4c852c19
huras <hurast@gmail.com>
2022-09-14 14:27:13 -0300

    update plugin header

--- wp-content/plugins/kdex-plugin/kdex-plugin.php
+++ wp-content/plugins/kdex-plugin/kdex-plugin.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name:       Anime Game Plugin
+ * Plugin Name:       KDex Plugin
  * Description:       Handle the basics with this plugin.
  * Version:           1.0.0
  * Requires at least: 5.2
@@ -11,7 +11,7 @@
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Update URI:        https://example.com/my-plugin/
  * Text Domain:       kdex-plugin
- * Domain Path:       /agg
+ * Domain Path:       /kdex
  */
 
 
